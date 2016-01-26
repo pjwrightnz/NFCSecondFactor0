82a9033a13893202609671b3a996487a1d95b24c
pjwrightnz <pauljwright80@gmail.com>
2016-01-26 10:26:12 +0000

    asd

---
+++ RegisterstrationActivity.java
@@ -0,0 +1,19 @@
+package com.example.paul.nfcsecondfactor0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class RegisterstrationActivity extends AppCompatActivity {
+
+    Button registerButton;
+    EditText userIDEditText, passwordEditText, reenterPasswordEditText, emailEditText;
+    ImageView nfcLogo, bTLogo, yorkLogo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registerstration);
+    }
+}
